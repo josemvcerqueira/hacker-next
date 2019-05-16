36130d66dd1c3531b77f53fcd0a0c13f9e0e121f
Jose Cerqueira <jose@origensmedia.com>
2019-05-16 10:02:55 -0700

    Fetched the stories from the api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
-import { Component } from "react";
+import { Component, Fragment } from "react";
+import fetch from "node-fetch";
+
+import Error from "next/error";
 
 class Index extends Component {
+	static async getInitialProps() {
+		let stories;
+		try {
+			const response = await fetch(
+				"https://node-hnapi.herokuapp.com/news?page=1"
+			);
+			stories = await response.json();
+		} catch (error) {
+			console.log(error);
+			stories = [];
+		}
+		return { stories };
+	}
+
 	render() {
-		return <div>Hacker Next</div>;
+		const { stories } = this.props;
+
+		if (!stories.length) {
+			return <Error statusCode={503} />;
+		}
+		return (
+			<Fragment>
+				<div>Hacker Next</div>
+				<div>
+					{stories.map(story => (
+						<h2 key={story.id}>{story.title}</h2>
+					))}
+				</div>
+			</Fragment>
+		);
 	}
 }
 
